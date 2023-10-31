408315f336e58e5fd0fa00c4794cbf0f7b1312b0
Kiryl Shchasny <folkbread@gmail.com>
2023-10-31 13:28:41 +0100

    refactor: changed constant with link

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export const LOCAL_STORAGE_TERM = 'prev-search-term';
 
-export const LINK_TO_PAGE =
-  'https://rolling-scopes-school.github.io/menromi-REACT2023Q4/';
+export const LINK_TO_PAGE = 'https://menromi.github.io/rsschool-menromi-2023/';
