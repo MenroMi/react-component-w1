fdedd9a4c18906a308376ee4e96c068fc3543ffc
Kiryl Shchasny <folkbread@gmail.com>
2023-11-06 16:56:33 +0100

    refactor: changed styles and card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,56 +1,31 @@
 import { FC } from 'react';
 import { Box } from '../shared';
 import styles from './Card.module.css';
-import { IAbility, IStats, IType } from '../../types/pokemonTypes';
+import { IPokemon } from '../../types/pokemonTypes';
 
-interface ICardProps {
-  id: number;
-  name: string;
-  abilities: IAbility[];
-  types: IType[];
-  stats: IStats[];
+interface ICardProps extends IPokemon {
+  isActive: boolean;
+  handleClickShowMore: (pokemon: IPokemon) => void;
 }
 
-const Card: FC<ICardProps> = ({ abilities, id, name, stats, types }) => {
-  const onIterateFields = (
-    fields: (IAbility[] | IType[] | IStats[])[],
-    names: string[]
-  ): JSX.Element[] => {
-    return fields.map((field, i) => {
-      return (
-        <li key={i}>
-          <span>{names[i]}</span>:{' '}
-          {field.map((fieldValue, i) => {
-            let actualValue: string;
-
-            if ('ability' in fieldValue) {
-              actualValue = fieldValue['ability'].name;
-            } else if ('stat' in fieldValue) {
-              actualValue = fieldValue['stat'].name;
-            } else {
-              actualValue = fieldValue['type'].name;
-            }
-
-            return i + 1 === field.length
-              ? `${actualValue}`
-              : `${actualValue}, `;
-          })}
-        </li>
-      );
-    });
-  };
-
-  const pokemonFieldName = ['ability', 'stat', 'type'];
-  const pokemonFieldValues = [abilities, stats, types];
-
+const Card: FC<ICardProps> = ({
+  handleClickShowMore,
+  isActive,
+  id,
+  name,
+  ...props
+}) => {
   return (
-    <Box className={styles.card}>
-      <p className={styles['card__id']}>{id}</p>
-      <p className={styles['card__name']}>{name}</p>
-
-      <ul className={styles['card__info']}>
-        {onIterateFields(pokemonFieldValues, pokemonFieldName)}
-      </ul>
+    <Box
+      className={`${styles.card} ${isActive && styles['card-active']}`}
+      onClick={() => handleClickShowMore({ id, name, ...props })}
+    >
+      <p className={styles['card__name']}>
+        {id}. {name}
+      </p>
+      <p className={styles['card__btn']}>
+        {isActive ? 'Hide Info' : 'Show More'}
+      </p>
     </Box>
   );
 };
