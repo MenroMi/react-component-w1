a4da589202f5ff6f54ce3a6290d21fc960b7bba0
Kiryl Shchasny <folkbread@gmail.com>
2023-11-06 16:59:45 +0100

    refactor: changes in loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,14 +1,13 @@
 import { Box, Overlay } from '../shared';
 import { GearLoaderIcon } from '../Icons';
-import styles from './Loader.module.css';
 
 interface ILoaderProps extends React.HTMLAttributes<HTMLDivElement> {
   [x: string]: unknown;
 }
 
-const Loader = (props: ILoaderProps) => {
+const Loader = ({ className, ...props }: ILoaderProps) => {
   return (
-    <Box className={styles.loader} {...props}>
+    <Box className={className} {...props}>
       <Overlay>
         <GearLoaderIcon />
       </Overlay>
