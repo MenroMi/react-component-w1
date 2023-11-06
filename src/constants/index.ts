c05d84b466c370898f7a4bfa167cb4ce4dad4302
Kiryl Shchasny <folkbread@gmail.com>
2023-11-06 15:59:15 +0100

    refactot: added more constants for specified elems

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,18 @@
+import { IInitialData, IUrlParams } from '../types';
+
 export const LOCAL_STORAGE_TERM = 'prev-search-term';
 
+export const UNKNOWN = 'unknown';
+
 export const HOMEPAGE_URL = 'https://menromi.github.io/rsschool-menromi-2023/';
+
+export const initialData: IInitialData = {
+  actualPage: 1,
+  nextPage: 2,
+  prevPage: 0,
+  offset: 0,
+  limit: 9,
+  totalCountPages: 0,
+};
+
+export const urlParams: IUrlParams = { page: 0, details: 0 };
